65bb7518ef6beac928dc16dcaf05f2ac4ba76895
quyixiao <2621048238@qq.com>
2021-12-06 12:44:23 +0800

    提交修改

--- src/main/java/com/lz/express/controller/ExpressController.java
+++ src/main/java/com/lz/express/controller/ExpressController.java
@@ -1,4 +1,27 @@
 package com.lz.express.controller;
 
+
+import com.alibaba.fastjson.JSON;
+import com.lz.express.entity.UserExpress;
+import com.lz.express.service.UserExpressService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
 public class ExpressController {
+
+
+    @Autowired
+    private UserExpressService userExpressService;
+
+    @RequestMapping("/test")
+    public String test() {
+        UserExpress userExpress = userExpressService.selectUserExpressById(1l);
+        log.info("===========" + JSON.toJSONString(userExpress));
+        return "tesxt";
+    }
+
 }
